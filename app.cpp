f21a865d38cc7f40469a87adeab84f034808ef70
zach <zacharymd14@gmail.com>
2024-06-30 21:00:58 +0200

    Added switch for menu selec

--- app.cpp
+++ app.cpp
@@ -6,9 +6,31 @@ int main(int argc, char *argv[]) {
 
   cout << "\n===== EXPENSE TRACKER =====\n";
 
+  // User input the menu he want to go to
   cout << "Menu selection : ";
   int menuSelection;
   cin >> menuSelection;
-  // cout << "Going into menu :" << menuSelection;
+
+  switch (menuSelection) {
+  case 1:
+    cout << "\nyou selected 1\n";
+    break;
+  case 2:
+    cout << "\nyou selected 2\n";
+    break;
+  case 3:
+    cout << "\nyou selected 3\n";
+    break;
+  case 4:
+    cout << "\nyou selected 4\n";
+    break;
+  case 5:
+    cout << "\nyou selected 5\n";
+    break;
+  default:
+    cout << "\nNah, wrong option mate...";
+    break;
+  }
+
   return 0;
 }
